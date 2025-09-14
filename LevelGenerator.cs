13b600c9a68701e6587e0dd8bc0704ea5247d3fe
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2025-09-14 22:01:09 +0530

    Update LevelGenerator.cs

--- LevelGenerator.cs
+++ LevelGenerator.cs
@@ -1,29 +1,123 @@
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject chunkPrefab; // Prefab for the level chunk
-    [SerializeField] int startingChunksAmount = 12; // Number of chunks to instantiate at the start
-    [SerializeField] Transform chunkParent; // Parent transform for instantiated chunks
-    [SerializeField] float chunkLength = 10f; // Length of each chunk
+    [Header("References")]
+    [SerializeField] CameraController cameraController;
+    [SerializeField] GameObject[] chunkPrefabs;
+    [SerializeField] GameObject checkPointChunkPrefab;
+
+    [SerializeField] Transform chunkParent;
+    [SerializeField] ScoreManager scoreManager;
+
+    [Header("Level Settings")]
+    [Tooltip("The amount of chunks westart with")]
+    [SerializeField] int startingChunksAmount = 12;
+    [SerializeField] int checkPointChunkInterval = 10;
+    [Tooltip("Do not change length vlaue unless chunk prefab size reflects change")]
+    [SerializeField] float chunkLength = 10f;
+    [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float minMoveSpeed = 2f;
+    [SerializeField] float maxMoveSpeed = 20f;
+    [SerializeField] float minGravityZ = -2.2f;
+    [SerializeField] float maxGravityZ = 2f;
+
+
+    List<GameObject> chunks = new List<GameObject>();
+    int chunksSpawned = 0;
 
     void Start()
     {
-        for (int i = 0; i < startingChunksAmount; i++) 
-        {
-            float spawnPositionZ;
-            if(i==0)
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else
-            {
-                spawnPositionZ = transform.position.z + (i * chunkLength);
-            }
-            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        SpawnStartingChunks();
+    }
+    void Update()
+    {
+        MoveChunks();
+    }
+    public void ChangeChunkMoveSpeed(float speedAmount)
+    {
+        float newMoveSpeed = moveSpeed + speedAmount;
+        newMoveSpeed = Mathf.Clamp(newMoveSpeed, minMoveSpeed, maxMoveSpeed);
 
-            Instantiate(chunkPrefab, transform.position, Quaternion.identity, chunkParent); // Instantiate the chunk prefab at the position of the LevelGenerator
+        if (newMoveSpeed != moveSpeed)
+        {
+            moveSpeed = newMoveSpeed;
+
+            float newGravityZ = Physics.gravity.z - speedAmount;
+            newGravityZ = Mathf.Clamp(newGravityZ, minGravityZ, maxGravityZ);
+
+            Physics.gravity = new Vector3(Physics.gravity.x, Physics.gravity.y, Physics.gravity.z - speedAmount);
+            cameraController.ChangeCameraFOV(speedAmount);
+        }
+    }
+
+    public void SpawnStartingChunks()
+    {
+        for (int i = 0; i < startingChunksAmount; i++)
+        {
+            SpawnChunk();
+        }
+    }
+    private void SpawnChunk()
+    {
+        float spawnPositionZ = CalculateSpawnPositionZ();
+        Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        GameObject chunkToSpawn = ChooseChunkToSpawn();
+        //chunkToSpawn = chunkPrefabs[Random.Range(0, chunkPrefabs.Length)];
+        GameObject newChunkGO = Instantiate(chunkToSpawn, chunkSpawnPos, Quaternion.identity, chunkParent);
+        chunks.Add(newChunkGO);
+        Chunk newChunk = newChunkGO.GetComponent<Chunk>();
+        newChunk.Init(this, scoreManager);
+       
+        chunksSpawned++;
+    }
+
+    private GameObject ChooseChunkToSpawn()
+    {
+        GameObject chunkToSpawn;
+        if (chunksSpawned % checkPointChunkInterval == 0)
+        {
+            // Spawn a checkpoint chunk
+            chunkToSpawn = checkPointChunkPrefab;
+        }
+        else
+        {
+            // Spawn a regular chunk
+            chunkToSpawn = chunkPrefabs[Random.Range(0, chunkPrefabs.Length)];
+        }
+
+        return chunkToSpawn;
+    }
+
+    float CalculateSpawnPositionZ()
+    {
+        float spawnPositionZ;
+        if (chunks.Count == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            //spawnPositionZ = transform.position.z + (i * chunkLength);
+            spawnPositionZ = chunks[chunks.Count - 1].transform.position.z + chunkLength;
+        }
+        return spawnPositionZ;
+    }
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Count; i++)
+        {
+            GameObject chunk = chunks[i];
+            chunk.transform.Translate(-transform.forward * moveSpeed * Time.deltaTime);
+
+            if (chunk.transform.position.z <= Camera.main.transform.position.z - chunkLength)
+            {
+                chunks.Remove(chunk);
+                Destroy(chunk);
+                SpawnChunk();
+            }
         }
     }
 }
+
